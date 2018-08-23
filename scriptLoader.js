bdaaead500e83a45a1a9af0d8662df93c08f1c23
Misha Marinenko <40549120+misha-marinenko-official@users.noreply.github.com>
2018-08-23 23:34:29 +0300

    Update scriptLoader.js

--- scriptLoader.js
+++ scriptLoader.js
@@ -4,6 +4,6 @@ This file loading the scripts from repo by calling a function.
 */
 function load(name){
 var script = document.createElement('script');
-script.src = name;
+script.src = "https://raw.githubusercontent.com/misha-marinenko-official/my-tmScripts/master/"+name;
 document.head.appendChild(script);
 }
