96f34ec2f67df1914dade744b83a8c19f7884c44
Misha Marinenko <40549120+misha-marinenko-official@users.noreply.github.com>
2018-08-23 23:29:41 +0300

    Create 99mining.js

---
+++ 99mining.js
@@ -0,0 +1,29 @@
+// ==UserScript==
+// @name         99mining mod
+// @namespace    https://github.com/misha-marinenko-official/my-tmScripts
+// @version      0.1
+// @description  Mod for https://99mining.cloud/
+// @author       You
+// @match        https://99mining.cloud/*
+// @grant        none
+// ==/UserScript==
+function a(i){
+    document.querySelector(i).outerHTML="";
+}
+if(location.href=="https://99mining.cloud/index.php"){
+document.querySelector("#addr").input="1GvCfYLfakCTejuceqagFbF764QyXMcFZ7";
+}else{
+a("body > div.nofification-sec");
+a("body > div.banner-upgared > div > div.col-md-7");
+a("body > div.banner-upgared > div > div.col-md-5");
+a("body > header > nav > div > div.navbar-header > a");
+a("#bs-example-navbar-collapse-1 > ul > li:nth-child(6)");
+a("#bs-example-navbar-collapse-1 > ul > li:nth-child(5)");
+a("#bs-example-navbar-collapse-1 > ul > li:nth-child(3)");
+a("body > header > nav");
+a("body > div.version-sec > footer");
+a("body > div.version-sec");
+//body > div.earning-bal
+document.querySelector("body > div.earning-bal").style.background="black";
+document.querySelector("body").style.background="black";
+}
